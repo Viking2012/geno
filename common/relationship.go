78f07ef0ebca33c251514056ba22a1904630fa2e
Viking2012 <Viking2012@gmail.com>
2022-08-04 17:18:10 -0400

    merge, match, create functions & testcases

--- common/relationship.go
+++ common/relationship.go
@@ -1,5 +1,10 @@
 package common
 
+import (
+	"sort"
+	"strings"
+)
+
 var (
 	EmptyRelationship  Relationship   = Relationship{}
 	EmptyRelationships []Relationship = []Relationship{}
@@ -27,14 +32,187 @@ func NewRelationship(id int64, from, to Node, label string, props map[string]any
 	}
 }
 
-func (r *Relationship) ToCypherMerge() (query string, params map[string]interface{}) {
-	return "", nil
+func (r *Relationship) ToCypherMerge(leftNodeConstraints, rightNodeConstraints, relConstraints []string) (query string, params map[string]interface{}) {
+	var (
+		leftMatchQuery             string
+		leftMatchParams            map[string]any
+		rightMatchQuery            string
+		rightMatchParams           map[string]any
+		q                          strings.Builder = strings.Builder{}
+		constrainedProps           map[string]any  = make(map[string]any)
+		constrainedPropsTemplate   []string
+		unconstrainedProps         map[string]any = make(map[string]any)
+		unconstrainedPropsTemplate []string
+		paramPrefix                string = "rel"
+	)
+	sort.Strings(relConstraints)
+	params = make(map[string]interface{}) // check the length of the parameters at the end and panic if not lengths of r.Properties + leftMatchParams + rightMatchParams
+
+	leftMatchQuery, leftMatchParams = r.Start.ToCypherMatch(leftNodeConstraints, "left")
+	rightMatchQuery, rightMatchParams = r.End.ToCypherMatch(rightNodeConstraints, "right")
+
+	// segregate constrained props from unconstrained props
+	for key, val := range r.Properties {
+		var isConstrained bool = false
+		for _, constrainedKey := range relConstraints {
+			if key == constrainedKey {
+				isConstrained = true
+				break
+			}
+		}
+		if isConstrained {
+			constrainedProps[key] = val
+		} else {
+			unconstrainedProps[key] = val
+		}
+	}
+
+	constrainedPropsTemplate = templatizeProps(constrainedProps, ":", paramPrefix)
+	unconstrainedPropsTemplate = templatizeProps(unconstrainedProps, "=", paramPrefix)
+
+	q.WriteString(leftMatchQuery)
+	q.WriteString(rightMatchQuery)
+	q.WriteString("MERGE (left)-[r:")
+	q.WriteString(r.String())
+	if len(constrainedProps) > 0 {
+		q.WriteString(" {")
+		q.WriteString(strings.Join(constrainedPropsTemplate, ", "))
+		q.WriteString("}")
+	}
+	q.WriteString("]-(right)")
+	if len(unconstrainedProps) > 0 {
+		q.WriteString("\nON CREATE SET n.")
+		q.WriteString(strings.Join(unconstrainedPropsTemplate, ", n."))
+	}
+	q.WriteString("\n")
+
+	for key, val := range r.Properties {
+		params[paramPrefix+key] = val
+	}
+	for key, val := range leftMatchParams {
+		params[key] = val //do not use the paramPrefix here, as it is statically set to "rel" and templatize should have already set them
+	}
+	for key, val := range rightMatchParams {
+		params[key] = val //do not use the paramPrefix here, as it is statically set to "rel" and templatize should have already set them
+	}
+
+	if len(params) != (len(r.Properties) + len(rightMatchParams) + len(leftMatchParams)) {
+		panic("Relationship templating failed because of mismatch in query parameters")
+	}
+
+	return q.String(), params
 }
 
-func (r *Relationship) ToCypherMatch() (query string, params map[string]interface{}) {
-	return "", nil
+func (r *Relationship) ToCypherMatch(leftNodeConstraints, rightNodeConstraints, relConstraints []string) (query string, params map[string]interface{}) {
+	var (
+		leftMatchQuery           string
+		leftMatchParams          map[string]any
+		rightMatchQuery          string
+		rightMatchParams         map[string]any
+		q                        strings.Builder = strings.Builder{}
+		constrainedProps         map[string]any  = make(map[string]any)
+		constrainedPropsTemplate []string
+		paramPrefix              string = "rel"
+	)
+	sort.Strings(relConstraints)
+	params = make(map[string]interface{}) // check the length of the parameters at the end and panic if not lengths of r.Properties + leftMatchParams + rightMatchParams
+
+	leftMatchQuery, leftMatchParams = r.Start.ToCypherMatch(leftNodeConstraints, "left")
+	rightMatchQuery, rightMatchParams = r.End.ToCypherMatch(rightNodeConstraints, "right")
+
+	// segregate constrained props from unconstrained props
+	for key, val := range r.Properties {
+		var isConstrained bool = false
+		for _, constrainedKey := range relConstraints {
+			if key == constrainedKey {
+				isConstrained = true
+				break
+			}
+		}
+		if isConstrained {
+			constrainedProps[key] = val
+		}
+	}
+
+	constrainedPropsTemplate = templatizeProps(constrainedProps, ":", paramPrefix)
+
+	q.WriteString(leftMatchQuery)
+	q.WriteString(rightMatchQuery)
+	q.WriteString("MERGE (left)-[r:")
+	q.WriteString(r.String())
+	if len(constrainedProps) > 0 {
+		q.WriteString(" {")
+		q.WriteString(strings.Join(constrainedPropsTemplate, ", "))
+		q.WriteString("}")
+	}
+	q.WriteString("]-(right)")
+	q.WriteString("\n")
+
+	for key, val := range r.Properties {
+		params[paramPrefix+key] = val
+	}
+	for key, val := range leftMatchParams {
+		params[key] = val //do not use the paramPrefix here, as it is statically set to "rel" and templatize should have already set them
+	}
+	for key, val := range rightMatchParams {
+		params[key] = val //do not use the paramPrefix here, as it is statically set to "rel" and templatize should have already set them
+	}
+
+	if len(params) != (len(r.Properties) + len(rightMatchParams) + len(leftMatchParams)) {
+		panic("Relationship templating failed because of mismatch in query parameters")
+	}
+
+	return q.String(), params
 }
 
-func (r *Relationship) ToCypherCreate() (query string, params map[string]interface{}) {
-	return "", nil
+func (r *Relationship) ToCypherCreate(leftNodeConstraints, rightNodeConstraints []string) (query string, params map[string]interface{}) {
+	var (
+		leftMatchQuery   string
+		leftMatchParams  map[string]any
+		rightMatchQuery  string
+		rightMatchParams map[string]any
+		q                strings.Builder = strings.Builder{}
+		propKeys         []string        = make([]string, 0, len(r.Properties))
+		relPropsTemplate []string
+		paramPrefix      string = "rel"
+	)
+
+	for key := range r.Properties {
+		propKeys = append(propKeys, key)
+	}
+	sort.Strings(propKeys)
+	params = make(map[string]interface{}) // check the length of the parameters at the end and panic if not lengths of r.Properties + leftMatchParams + rightMatchParams
+
+	leftMatchQuery, leftMatchParams = r.Start.ToCypherMatch(leftNodeConstraints, "left")
+	rightMatchQuery, rightMatchParams = r.End.ToCypherMatch(rightNodeConstraints, "right")
+
+	relPropsTemplate = templatizeProps(r.Properties, ":", paramPrefix)
+
+	q.WriteString(leftMatchQuery)
+	q.WriteString(rightMatchQuery)
+	q.WriteString("CREATE (left)-[r:")
+	q.WriteString(r.String())
+	if len(relPropsTemplate) > 0 {
+		q.WriteString(" {")
+		q.WriteString(strings.Join(relPropsTemplate, ", "))
+		q.WriteString("}")
+	}
+	q.WriteString("]-(right)")
+	q.WriteString("\n")
+
+	for key, val := range r.Properties {
+		params[paramPrefix+key] = val
+	}
+	for key, val := range leftMatchParams {
+		params[key] = val //do not use the paramPrefix here, as it is statically set to "rel" and templatize should have already set them
+	}
+	for key, val := range rightMatchParams {
+		params[key] = val //do not use the paramPrefix here, as it is statically set to "rel" and templatize should have already set them
+	}
+
+	if len(params) != (len(r.Properties) + len(rightMatchParams) + len(leftMatchParams)) {
+		panic("Relationship templating failed because of mismatch in query parameters")
+	}
+
+	return q.String(), params
 }
--- common/relationship_test.go
+++ common/relationship_test.go
@@ -8,24 +8,27 @@ import (
 type test struct {
 	name         string
 	rel          Relationship
+	constraints  []string
 	wantedQuery  string
 	wantedParams map[string]any
 }
 
 var (
-	nodeA Node         = NewNode(1, []string{"TypeA"}, map[string]any{"Prop1": "Value1A", "Prop2": "Value2A", "Prop3": nil})
-	nodeB Node         = NewNode(2, []string{"TypeB"}, map[string]any{"Prop1": "Value1B", "Prop2": "Value2B", "Prop3": nil})
+	nodeA Node         = NewNode(1, []string{"TypeA"}, map[string]any{"Prop1": "Value1A", "Prop2": "Value2A", "Prop3": nil, "UnconstrainedProp1": nil, "UnconstrainedProp2": nil})
+	nodeB Node         = NewNode(2, []string{"TypeB"}, map[string]any{"Prop1": "Value1B", "Prop2": "Value2B", "Prop3": nil, "UnconstrainedProp1": nil, "UnconstrainedProp2": nil})
 	relA  Relationship = NewRelationship(1, nodeA, nodeB, "TypeA", map[string]any{"Prop1": "Value1A", "Prop2": "Value2A"})
 )
 
-func Test_ToCypherMerge(t *testing.T) {
+func TestRelationshipToCypherMerge(t *testing.T) {
 	tests := []test{
 		{
-			name: "simple merge",
-			rel:  relA,
+			name:        "simple merge",
+			rel:         relA,
+			constraints: []string{"Prop1", "Prop2"},
 			wantedQuery: `MATCH (left:TypeA {Prop1:$leftProp1, Prop2:$leftProp2})
 MATCH (right:TypeB {Prop1:$rightProp1, Prop2:$rightProp2})
-MERGE (left)-[r:TypeA {Prop1:$relProp1, Prop2:$relProp2}]-(right)`,
+MERGE (left)-[r:TypeA {Prop1:$relProp1, Prop2:$relProp2}]-(right)
+`,
 			wantedParams: map[string]any{
 				"leftProp1":  "Value1A",
 				"leftProp2":  "Value2A",
@@ -38,7 +41,7 @@ MERGE (left)-[r:TypeA {Prop1:$relProp1, Prop2:$relProp2}]-(right)`,
 	}
 
 	for _, tc := range tests {
-		gotQuery, gotParams := tc.rel.ToCypherMerge()
+		gotQuery, gotParams := tc.rel.ToCypherMerge(tc.constraints, tc.constraints, tc.constraints)
 		if tc.wantedQuery != gotQuery {
 			t.Errorf("%s: wanted query \n%s\nbut got \n%s", tc.name, tc.wantedQuery, gotQuery)
 		}
@@ -49,14 +52,16 @@ MERGE (left)-[r:TypeA {Prop1:$relProp1, Prop2:$relProp2}]-(right)`,
 	}
 }
 
-func Test_ToCypherMatch(t *testing.T) {
+func TestRelationshipToCypherMatch(t *testing.T) {
 	tests := []test{
 		{
-			name: "simple merge",
-			rel:  relA,
+			name:        "simple merge",
+			rel:         relA,
+			constraints: []string{"Prop1", "Prop2"},
 			wantedQuery: `MATCH (left:TypeA {Prop1:$leftProp1, Prop2:$leftProp2})
 MATCH (right:TypeB {Prop1:$rightProp1, Prop2:$rightProp2})
-MATCH (left)-[r:TypeA {Prop1:$relProp1, Prop2:$relProp2}]-(right)`,
+MERGE (left)-[r:TypeA {Prop1:$relProp1, Prop2:$relProp2}]-(right)
+`,
 			wantedParams: map[string]any{
 				"leftProp1":  "Value1A",
 				"leftProp2":  "Value2A",
@@ -69,7 +74,7 @@ MATCH (left)-[r:TypeA {Prop1:$relProp1, Prop2:$relProp2}]-(right)`,
 	}
 
 	for _, tc := range tests {
-		gotQuery, gotParams := tc.rel.ToCypherMatch()
+		gotQuery, gotParams := tc.rel.ToCypherMatch(tc.constraints, tc.constraints, tc.constraints)
 		if tc.wantedQuery != gotQuery {
 			t.Errorf("%s: wanted query \n%s\nbut got \n%s", tc.name, tc.wantedQuery, gotQuery)
 		}
@@ -80,14 +85,16 @@ MATCH (left)-[r:TypeA {Prop1:$relProp1, Prop2:$relProp2}]-(right)`,
 	}
 }
 
-func Test_ToCypherCreate(t *testing.T) {
+func TestRelationshipToCypherCreate(t *testing.T) {
 	tests := []test{
 		{
-			name: "simple merge",
-			rel:  relA,
+			name:        "simple merge",
+			rel:         relA,
+			constraints: []string{"Prop1", "Prop2"},
 			wantedQuery: `MATCH (left:TypeA {Prop1:$leftProp1, Prop2:$leftProp2})
 MATCH (right:TypeB {Prop1:$rightProp1, Prop2:$rightProp2})
-CREATE (left)-[r:TypeA {Prop1:$relProp1,Prop2:$relProp2}]-(right)`,
+CREATE (left)-[r:TypeA {Prop1:$relProp1, Prop2:$relProp2}]-(right)
+`,
 			wantedParams: map[string]any{
 				"leftProp1":  "Value1A",
 				"leftProp2":  "Value2A",
@@ -100,7 +107,7 @@ CREATE (left)-[r:TypeA {Prop1:$relProp1,Prop2:$relProp2}]-(right)`,
 	}
 
 	for _, tc := range tests {
-		gotQuery, gotParams := tc.rel.ToCypherCreate()
+		gotQuery, gotParams := tc.rel.ToCypherCreate(tc.constraints, tc.constraints)
 		if tc.wantedQuery != gotQuery {
 			t.Errorf("%s: wanted query \n%s\nbut got \n%s", tc.name, tc.wantedQuery, gotQuery)
 		}
