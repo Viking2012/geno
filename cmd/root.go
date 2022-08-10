a90389639f35bbd54b56ff598981004cb1af75f9
Viking2012 <Viking2012@gmail.com>
2022-08-10 15:13:07 -0400

    move config from json to yaml

--- cmd/root.go
+++ cmd/root.go
@@ -64,7 +64,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.geno.json)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.geno.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -83,7 +83,7 @@ func initConfig() {
 
 		// Search config in home directory with name ".geno" (without extension).
 		viper.AddConfigPath(home)
-		viper.SetConfigType("json")
+		viper.SetConfigType("yaml")
 		viper.SetConfigName(".geno")
 	}
 
