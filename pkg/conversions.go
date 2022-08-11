4c4b995a610dac14f690359ee267e3eefea02443
Viking2012 <Viking2012@gmail.com>
2022-08-11 14:46:58 -0400

    move conversions of json to geno structs to its own file

---
+++ pkg/conversions.go
@@ -0,0 +1,59 @@
+package pkg
+
+import (
+	"encoding/json"
+
+	"github.com/Viking2012/geno/geno"
+)
+
+type readNode struct {
+	Id     int64          `json:"identity"`
+	Labels []string       `json:"labels"`
+	Props  map[string]any `json:"properties"`
+}
+
+type readRelationship struct {
+	Id         int64          `json:"identity"`
+	Start      int64          `json:"start"`
+	End        int64          `json:"end"`
+	Label      string         `json:"type"`
+	Properties map[string]any `json:"properties"`
+}
+type readGraph struct {
+	Nodes []readNode         `json:"nodes"`
+	Rels  []readRelationship `json:"rels"`
+}
+
+type graph struct {
+	Nodes         []geno.Node
+	Relationships []geno.Relationship
+}
+
+func getGraphFromJson(raw []byte) (g graph, err error) {
+	var js readGraph
+
+	if err := json.Unmarshal(raw, &js); err != nil {
+		return g, err
+	}
+
+	g.Nodes = make([]geno.Node, len(js.Nodes))
+	for i := range js.Nodes {
+		rawN := js.Nodes[i]
+		g.Nodes[i] = geno.NewNode(rawN.Id, rawN.Labels, rawN.Props)
+	}
+
+	g.Relationships = make([]geno.Relationship, len(js.Rels))
+	for i := range js.Rels {
+		rawR := js.Rels[i]
+		start, err := findNodeById(rawR.Start, g.Nodes)
+		if err != nil {
+			return g, err
+		}
+		end, err := findNodeById(rawR.End, g.Nodes)
+		if err != nil {
+			return g, err
+		}
+		g.Relationships[i] = geno.NewRelationship(rawR.Id, start, end, rawR.Label, rawR.Properties)
+	}
+	return g, nil
+}
--- pkg/import.go
+++ pkg/import.go
@@ -1,65 +1,12 @@
 package pkg
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/Viking2012/geno/geno"
 	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 )
-
-type jsonNode struct {
-	Id     int64          `json:"identity"`
-	Labels []string       `json:"labels"`
-	Props  map[string]any `json:"properties"`
-}
-
-type jsonRelationship struct {
-	Id         int64          `json:"identity"`
-	Start      int64          `json:"start"`
-	End        int64          `json:"end"`
-	Label      string         `json:"type"`
-	Properties map[string]any `json:"properties"`
-}
-type jsonFile struct {
-	Nodes []jsonNode         `json:"nodes"`
-	Rels  []jsonRelationship `json:"rels"`
-}
-
-type Graph struct {
-	Nodes         []geno.Node
-	Relationships []geno.Relationship
-}
-
-func getGraphFromJson(raw []byte) (g Graph, err error) {
-	var js jsonFile
-
-	if err := json.Unmarshal(raw, &js); err != nil {
-		return g, err
-	}
-
-	g.Nodes = make([]geno.Node, len(js.Nodes))
-	for i := range js.Nodes {
-		rawN := js.Nodes[i]
-		g.Nodes[i] = geno.NewNode(rawN.Id, rawN.Labels, rawN.Props)
-	}
-
-	g.Relationships = make([]geno.Relationship, len(js.Rels))
-	for i := range js.Rels {
-		rawR := js.Rels[i]
-		start, err := findNodeById(rawR.Start, g.Nodes)
-		if err != nil {
-			return g, err
-		}
-		end, err := findNodeById(rawR.End, g.Nodes)
-		if err != nil {
-			return g, err
-		}
-		g.Relationships[i] = geno.NewRelationship(rawR.Id, start, end, rawR.Label, rawR.Properties)
-	}
-	return g, nil
-}
 
 func findNodeById(id int64, nodes []geno.Node) (geno.Node, error) {
 	for i := range nodes {
@@ -72,7 +19,7 @@ func findNodeById(id int64, nodes []geno.Node) (geno.Node, error) {
 
 func ImportJson(uri, database, username, password, filepath string) error {
 	// raw, err := os.ReadFile(path.Join("test_data", "test.json"))
-	var graph Graph
+	var graph graph
 
 	raw, err := os.ReadFile(filepath)
 	if err != nil {
