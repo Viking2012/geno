bff14a9a4d5df9be0d15dd6625694b61d6776d4c
Viking2012 <Viking2012@gmail.com>
2022-08-15 15:13:14 -0400

    only execute geno

--- main.go
+++ main.go
@@ -21,20 +21,56 @@ THE SOFTWARE.
 */
 package main
 
-import (
-	"github.com/Viking2012/geno/pkg"
-)
+import "github.com/Viking2012/geno/cmd"
+
+// type TestConfiguration struct {
+// 	Server      string                      `json:"server"`
+// 	Database    string                      `json:"database"`
+// 	Username    string                      `json:"username"`
+// 	Constraints map[string]geno.Constraints `json:"constraints"`
+// }
 
 func main() {
 	// as a CLI, this will ultimately be the only command within the application that runs
-	// cmd.Execute()
+	cmd.Execute()
 
-	err := pkg.ImportJson("localhost:7687", "geno", "geno", "genopw", "./test/test.json")
-	if err != nil {
-		panic(err)
-	}
+	// raw, err := ioutil.ReadFile(path.Join(".", "test", "test_config.json"))
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	// session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeRead, DatabaseName: database})
-	// defer session.Close()
+	// _, err = pkg.ConfigFromBytes(raw)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
+	// fmt.Println("server:", cfg.Server)
+	// fmt.Println("database:", cfg.Database)
+	// for db, cons := range cfg.Constraints {
+	// 	fmt.Println(db)
+	// 	fmt.Println("NODE-UNIQUENESS")
+	// 	for _, c := range cons.NodeUniqueness {
+	// 		fmt.Println(c)
+	// 	}
+	// 	fmt.Println("NODE-KEYS")
+	// 	for _, c := range cons.NodeKeys {
+	// 		fmt.Println(c)
+	// 	}
+	// 	fmt.Println("NODE-PROP-EXISTENCE")
+	// 	for _, c := range cons.NodePropertyExistence {
+	// 		fmt.Println(c)
+	// 	}
+	// 	fmt.Println("Relationship-UNIQUENESS")
+	// 	for _, c := range cons.RelationshipUniqueness {
+	// 		fmt.Println(c)
+	// 	}
+	// 	fmt.Println("Relationship-KEYS")
+	// 	for _, c := range cons.RelationshipKeys {
+	// 		fmt.Println(c)
+	// 	}
+	// 	fmt.Println("Relationship-PROP-EXISTENCE")
+	// 	for _, c := range cons.RelationshipPropertyExistence {
+	// 		fmt.Println(c)
+	// 	}
+	// }
 }
